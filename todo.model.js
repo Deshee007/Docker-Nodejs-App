b1cd04cf2c83b48e4d8a8372e900d0883afaa64e
Adeshile <segundeshee@icloud.com>
2023-02-02 17:14:20 +0000

    modofied files

--- todo.model.js
+++ todo.model.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;let Todo = new Schema({
+const Schema = mongoose.Schema;
+
+let Todo = new Schema({
     todo_description: {
         type: String
     },
@@ -12,4 +14,6 @@ const Schema = mongoose.Schema;let Todo = new Schema({
     todo_completed: {
         type: Boolean
     }
-});module.exports = mongoose.model('Todo', Todo);
+});
+
+module.exports = mongoose.model('Todo', Todo);
